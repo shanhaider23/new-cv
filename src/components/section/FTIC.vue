a807157af16247d4a83cb0f3c0d3d9d221e70d85
shanhaider23 <shan.haider@gmail.com>
2024-11-15 14:07:21 +0100

    feat: updated text

--- src/components/section/FTIC.vue
+++ src/components/section/FTIC.vue
@@ -1,6 +1,78 @@
 <template>
 	<div>
 		<div class="ftic">
+			<a class="button" href="https://crypto-core.vercel.app/" target="_blank">Crypto Core</a>
+			<p>An interactive platform for monitoring cryptocurrency prices and trends with a seamless, responsive user experience.</p>
+			<div class="expirience-job__list">
+				<div class="expirience-job__item">React</div>
+				<div class="expirience-job__item">Material UI</div>
+				<div class="expirience-job__item">Firebase</div>
+			</div>
+		</div>
+		<div class="ftic">
+			<a class="button" href="https://workout-map-psi.vercel.app/" target="_blank">Workout Map</a>
+			<p>A complete workout monitoring app.</p>
+			<div class="expirience-job__list">
+				<div class="expirience-job__item">Vanilla.js</div>
+				<div class="expirience-job__item">Tailwind CSS</div>
+			</div>
+		</div>
+		<div class="ftic">
+			<a class="button" href="https://travel-wise-five.vercel.app/" target="_blank">Travel App</a>
+			<p>An app for planning trips, creating itineraries, collaborating, and documenting travel experiences.</p>
+			<div class="expirience-job__list">
+				<div class="expirience-job__item">React</div>
+				<div class="expirience-job__item">Maps</div>
+			</div>
+		</div>
+		<div class="ftic">
+			<a class="button" href="https://world-weather-shan.vercel.app/" target="_blank">World Weather</a>
+			<p>
+				A responsive weather app, delivering real-time global The Considerate Consumer | Hamburg, Germany weather data, highlighting dynamic
+				UI and API integration skills.
+			</p>
+			<div class="expirience-job__list">
+				<div class="expirience-job__item">React</div>
+				<div class="expirience-job__item">Maps</div>
+				<div class="expirience-job__item">TypeScript</div>
+			</div>
+		</div>
+		<div class="ftic">
+			<a class="button" href="https://weather-app-shan.vercel.app/" target="_blank">Weather App </a>
+			<p>A weather app offering real-time temperature, humidity, and wind speed for cities worldwide.</p>
+			<div class="expirience-job__list">
+				<div class="expirience-job__item">Vue3</div>
+				<div class="expirience-job__item">Vite</div>
+				<div class="expirience-job__item">CSS</div>
+			</div>
+		</div>
+		<div class="ftic">
+			<a class="button" href="https://recipe-hub-five.vercel.app/" target="_blank">Recipe-hub</a>
+			<p>A user-friendly app for searching, adding, and bookmarking recipes by name or ingredient.</p>
+			<div class="expirience-job__list">
+				<div class="expirience-job__item">Vanilla.js</div>
+				<div class="expirience-job__item">Tailwind CSS</div>
+			</div>
+		</div>
+		<div class="ftic">
+			<a class="button" href="https://currency-converter4u.vercel.app/" target="_blank">Currency Convert</a>
+			<p>A user-friendly app for real-time conversions across 32 currencies.</p>
+			<div class="expirience-job__list">
+				<div class="expirience-job__item">React</div>
+				<div class="expirience-job__item">Redux</div>
+				<div class="expirience-job__item">Tailwind CSS</div>
+			</div>
+		</div>
+		<div class="ftic">
+			<a class="button" href="https://blog-app-write-gold.vercel.app/" target="_blank">Blog</a>
+			<p>A blog app, demonstrating Appwrite integration for backend services.</p>
+			<div class="expirience-job__list">
+				<div class="expirience-job__item">React</div>
+				<div class="expirience-job__item">Appwrite</div>
+				<div class="expirience-job__item">Tailwind CSS</div>
+			</div>
+		</div>
+		<div class="ftic">
 			<a class="button" href="https://www.dianox.com/" target="_blank">Dianox</a>
 			<div class="expirience-job__list">
 				<div class="expirience-job__item">Next.js</div>
@@ -11,16 +83,10 @@
 				<div class="expirience-job__item">MongoDB</div>
 			</div>
 		</div>
+
 		<div class="ftic">
-			<a class="button" href="https://crypto-core.vercel.app/" target="_blank">Crypto Core</a>
-			<div class="expirience-job__list">
-				<div class="expirience-job__item">React,</div>
-				<div class="expirience-job__item">Material UI</div>
-				<div class="expirience-job__item">Firebase</div>
-			</div>
-		</div>
-		<div class="ftic">
-			<a class="button" href="https://staging-simply-name-it.herokuapp.com/profile" target="_blank">Simply Name it</a>
+			<a class="button" href="https://github.com/HackYourFuture-CPH/simply-name.it" target="_blank">Simply Name it</a>
+			<p>A collaborative app for candidate ranking on decision- making boards.</p>
 			<div class="expirience-job__list">
 				<div class="expirience-job__item">React</div>
 				<div class="expirience-job__item">Node</div>
@@ -30,6 +96,7 @@
 		</div>
 		<div class="ftic">
 			<a class="button" href="https://github.com/shanhaider23/masterclass-hyf-HYF-homework" target="_blank">Meal App</a>
+			<p>A meal app developed with React, C#/.NET, and Azure, featuring meals, invite guests, and manage events.</p>
 			<div class="expirience-job__list">
 				<div class="expirience-job__item">React</div>
 				<div class="expirience-job__item">C#/.NET</div>
@@ -39,6 +106,7 @@
 		</div>
 		<div class="ftic">
 			<a class="button" href="https://github.com/shanhaider23/meal-sharing" target="_blank">Meal Sharing App</a>
+			<p>A app, allowing users to host meals, invite guests.</p>
 			<div class="expirience-job__list">
 				<div class="expirience-job__item">React</div>
 				<div class="expirience-job__item">Node</div>
@@ -48,6 +116,7 @@
 		</div>
 		<div class="ftic">
 			<a class="button" href="https://expenses-spliting-app.vercel.app/" target="_blank">Expense Splitting App</a>
+			<p>An app for easy expense sharing that calculates each person’s share in groups for transparent cost distribution.</p>
 			<div class="expirience-job__list">
 				<div class="expirience-job__item">React</div>
 				<div class="expirience-job__item">Node</div>
@@ -56,20 +125,14 @@
 			</div>
 		</div>
 		<div class="ftic">
-			<a class="button" href="https://admin-panel-shan.vercel.app/" target="_blank">Admin Panel</a>
+			<a class="button" href="https://admin-panel-shan.vercel.app/" target="_blank">Finance Admin Panel</a>
+			<p>An app for easy expense sharing that calculates each person’s share in groups for transparent cost distribution.</p>
 			<div class="expirience-job__list">
 				<div class="expirience-job__item">React</div>
 				<div class="expirience-job__item">Chart.js</div>
 			</div>
 		</div>
-		<div class="ftic">
-			<a class="button" href="https://weather-app-shan.vercel.app/" target="_blank">Weather App </a>
-			<div class="expirience-job__list">
-				<div class="expirience-job__item">Vue3</div>
-				<div class="expirience-job__item">Vite</div>
-				<div class="expirience-job__item">CSS</div>
-			</div>
-		</div>
+
 		<div class="ftic">
 			<a class="button" href="https://www.ellebaek-contact.com/" target="_blank">Ellebæk Contact Network</a>
 			<a class="button" href="https://www.rentbike.nu/" target="_blank">Merry Cykler</a>
--- src/components/section/Work.vue
+++ src/components/section/Work.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="work">
 		<Company name="Render" post="Frontend Developer" :from="new Date(2022, 5)">
-			<li>Continuously develop dynamic and responsive user interfaces using Vue.js, React, and JavaScript.</li>
+			<li>Continuously develop dynamic and responsive user interfaces using React, and TypeScript.</li>
 			<li>Consistently use Element UI to create visually appealing and user-friendly interfaces.</li>
 			<li>
-				Implemented and maintain new features on SaaS software using advanced Vue.js skills, resulting in a highly intuitive and user-friendly
-				interface that consistently receives praise from client companies for enhancing user engagement and satisfaction.
+				Implemented and maintain new features on SaaS software using advanced Vue.js and JavaScript skills, resulting in a highly intuitive
+				and user-friendly interface that consistently receives praise from client companies for enhancing user engagement and satisfaction.
 			</li>
 			<li>Conduct Cypress E2E and Component testing to ensure the reliability and accuracy of application functionalities.</li>
 			<li>Identify and resolve errors and bugs to enhance and stabilize the user experience.</li>
@@ -54,6 +54,7 @@
 			<li>Built and managed the full-stack web app from scratch by using technologies Next.js, TypeScript, AWS and MongoDB.</li>
 			<li>Worked with a UI/UX designer and the CTO to plan, write, and debug the web application.</li>
 			<li>Integrated the app with REST APIs for Google Maps, social media logins, payment processors, and other services.</li>
+			<li>Managed time-sensitive updates, including content changes and database upgrades.</li>
 
 			<div class="expirience-job__projects">
 				<a href="https://www.dianox.com/" target="_blank" rel="noopener noreferrer"
@@ -74,18 +75,6 @@
 				<div class="expirience-job__item">AWS</div>
 				<div class="expirience-job__item">Git</div>
 				<div class="expirience-job__item">MongoDB</div>
-			</div>
-		</Company>
-		<Company name="Cellari" post="Frontend Developer" :from="new Date(2021, 12)" :to="new Date(2022, 5)">
-			<li>Worked with JavaScript (Vanilla.js), Docker, CSS, and HTML.</li>
-			<li>Managed time-sensitive updates, including content changes and database upgrades.</li>
-			<li>Improved existing platform and user interface features while developing new features and initiatives.</li>
-			<div class="expirience-job__list">
-				<div class="expirience-job__item">Vanilla.js</div>
-				<div class="expirience-job__item">CSS</div>
-				<div class="expirience-job__item">Docker</div>
-				<div class="expirience-job__item">GitLab</div>
-				<div class="expirience-job__item">HTML</div>
 			</div>
 		</Company>
 		<Company name="The Considerate Consumer" post="Frontend Developer" contract :from="new Date(2020, 5)" :to="new Date(2021, 2)">
@@ -150,24 +139,6 @@
 								d="M715.648 625.152 670.4 579.904l90.496-90.56c75.008-74.944 85.12-186.368 22.656-248.896-62.528-62.464-173.952-52.352-248.96 22.656L444.16 353.6l-45.248-45.248 90.496-90.496c100.032-99.968 251.968-110.08 339.456-22.656 87.488 87.488 77.312 239.424-22.656 339.456l-90.496 90.496zm-90.496 90.496-90.496 90.496C434.624 906.112 282.688 916.224 195.2 828.8c-87.488-87.488-77.312-239.424 22.656-339.456l90.496-90.496 45.248 45.248-90.496 90.56c-75.008 74.944-85.12 186.368-22.656 248.896 62.528 62.464 173.952 52.352 248.96-22.656l90.496-90.496 45.248 45.248zm0-362.048 45.248 45.248L398.848 670.4 353.6 625.152 625.152 353.6z"
 							></path></svg
 						>Ellebæk Contact Network
-					</span></a
-				>
-			</div>
-		</Company>
-		<Company name="MTI Education Center" post="Frontend Developer" volunteering :from="new Date(2020, 4)">
-			<li>
-				I'm volunteering as a front-end developer for MTI Education Center, maintaining their WordPress website, and handling design and
-				educational content updates.
-			</li>
-			<div data-v-5230a472="" class="expirience-job__projects">
-				<a href="https://www.madinatulilm.dk/" target="_blank" rel="noopener noreferrer"
-					><span>
-						<svg data-v-5230a472="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" style="width: 1em; margin-right: 0.25em">
-							<path
-								fill="currentColor"
-								d="M715.648 625.152 670.4 579.904l90.496-90.56c75.008-74.944 85.12-186.368 22.656-248.896-62.528-62.464-173.952-52.352-248.96 22.656L444.16 353.6l-45.248-45.248 90.496-90.496c100.032-99.968 251.968-110.08 339.456-22.656 87.488 87.488 77.312 239.424-22.656 339.456l-90.496 90.496zm-90.496 90.496-90.496 90.496C434.624 906.112 282.688 916.224 195.2 828.8c-87.488-87.488-77.312-239.424 22.656-339.456l90.496-90.496 45.248 45.248-90.496 90.56c-75.008 74.944-85.12 186.368-22.656 248.896 62.528 62.464 173.952 52.352 248.96-22.656l90.496-90.496 45.248 45.248zm0-362.048 45.248 45.248L398.848 670.4 353.6 625.152 625.152 353.6z"
-							></path></svg
-						>MTI
 					</span></a
 				>
 			</div>
